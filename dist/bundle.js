f8f475228d29afdd6ebe9c866ed4698a1dbd1554
jhonc <jiri.honc@mgm-tp.com>
2017-08-09 20:44:21 +0200

    Procfile

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,9 +6,9 @@
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
@@ -32,9 +32,6 @@
 /******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// identity function for calling harmony imports with the correct context
-/******/ 	__webpack_require__.i = function(value) { return value; };
 /******/
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
@@ -63,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -78,18 +75,30 @@ module.exports = React;
 
 "use strict";
 
-var __assign = (this && this.__assign) || Object.assign || function(t) {
-    for (var s, i = 1, n = arguments.length; i < n; i++) {
-        s = arguments[i];
-        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-            t[p] = s[p];
-    }
-    return t;
-};
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __webpack_require__(0);
+const ReactDOM = __webpack_require__(2);
+const CostCalculator_1 = __webpack_require__(3);
+ReactDOM.render(React.createElement(CostCalculator_1.default, { label: "Calculate trip costs" }), document.getElementById("example"));
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = ReactDOM;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
 const React = __webpack_require__(0);
 // import { CostData } from "../functions/TripCost";
 const TripCost_1 = __webpack_require__(4);
-const CostsForm_1 = __webpack_require__(3);
+const CostsForm_1 = __webpack_require__(5);
 class CostCalculator extends React.Component {
     constructor(props) {
         super(props);
@@ -105,9 +114,9 @@ class CostCalculator extends React.Component {
     formChanged(event) {
         const target = event.target;
         const name = target.name;
-        let formValues = __assign({}, this.state, { [name]: target.value });
+        let formValues = Object.assign({}, this.state, { [name]: target.value });
         let cost = TripCost_1.default(formValues);
-        this.setState(__assign({}, formValues, { ['costPerPerson']: cost }));
+        this.setState(Object.assign({}, formValues, { ['costPerPerson']: cost }));
     }
     render() {
         const dist = this.state.distance;
@@ -123,53 +132,7 @@ class CostCalculator extends React.Component {
                 costs)));
     }
 }
-Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = CostCalculator;
-
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports) {
-
-module.exports = ReactDOM;
-
-/***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-const React = __webpack_require__(0);
-class CostsForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
-    handleInputChange(event) {
-        this.props.handleInputChange(event);
-    }
-    render() {
-        return (React.createElement("fieldset", null,
-            React.createElement("div", { className: "row" },
-                React.createElement("div", { className: "col-md-4" },
-                    React.createElement("label", { htmlFor: "distance" }, "total distance (there and back):")),
-                React.createElement("div", { className: "col-md-8" },
-                    React.createElement("input", { type: "number", value: this.props.distance, onChange: this.handleInputChange, id: "distance", name: "distance" })),
-                React.createElement("div", { className: "col-md-4" },
-                    React.createElement("label", { htmlFor: "persons" }, "num. of persons:")),
-                React.createElement("div", { className: "col-md-8" },
-                    React.createElement("input", { type: "number", value: this.props.persons, onChange: this.handleInputChange, id: "persons", name: "persons" })),
-                React.createElement("div", { className: "col-md-4" },
-                    React.createElement("label", { htmlFor: "consumtion" }, "car consumption:")),
-                React.createElement("div", { className: "col-md-8" },
-                    React.createElement("input", { type: "number", value: this.props.consumtion, onChange: this.handleInputChange, id: "consumtion", name: "consumtion" })),
-                React.createElement("div", { className: "col-md-4" },
-                    React.createElement("label", { htmlFor: "fuelCosts" }, "fuel cost:")),
-                React.createElement("div", { className: "col-md-8" },
-                    React.createElement("input", { type: "number", value: this.props.fuelCosts, onChange: this.handleInputChange, id: "fuelCosts", name: "fuelCosts" })))));
-    }
-}
-exports.CostsForm = CostsForm;
 
 
 /***/ }),
@@ -178,6 +141,7 @@ exports.CostsForm = CostsForm;
 
 "use strict";
 
+Object.defineProperty(exports, "__esModule", { value: true });
 class CostData {
     constructor(distance, persons, consumtion, fuelCosts) {
         this.distance = distance;
@@ -194,7 +158,6 @@ function default_1(data) {
     let allCosts = (data.distance / 100) * data.consumtion * data.fuelCosts;
     return allCosts / data.persons;
 }
-Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = default_1;
 /*
 
@@ -227,10 +190,38 @@ export class TripCost extends CostData {
 
 "use strict";
 
+Object.defineProperty(exports, "__esModule", { value: true });
 const React = __webpack_require__(0);
-const ReactDOM = __webpack_require__(2);
-const CostCalculator_1 = __webpack_require__(1);
-ReactDOM.render(React.createElement(CostCalculator_1.default, { label: "Calculate trip costs" }), document.getElementById("example"));
+class CostsForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(event) {
+        this.props.handleInputChange(event);
+    }
+    render() {
+        return (React.createElement("fieldset", null,
+            React.createElement("div", { className: "row" },
+                React.createElement("div", { className: "col-md-4" },
+                    React.createElement("label", { htmlFor: "distance" }, "total distance (there and back):")),
+                React.createElement("div", { className: "col-md-8" },
+                    React.createElement("input", { type: "number", value: this.props.distance, onChange: this.handleInputChange, id: "distance", name: "distance" })),
+                React.createElement("div", { className: "col-md-4" },
+                    React.createElement("label", { htmlFor: "persons" }, "num. of persons:")),
+                React.createElement("div", { className: "col-md-8" },
+                    React.createElement("input", { type: "number", value: this.props.persons, onChange: this.handleInputChange, id: "persons", name: "persons" })),
+                React.createElement("div", { className: "col-md-4" },
+                    React.createElement("label", { htmlFor: "consumtion" }, "car consumption:")),
+                React.createElement("div", { className: "col-md-8" },
+                    React.createElement("input", { type: "number", value: this.props.consumtion, onChange: this.handleInputChange, id: "consumtion", name: "consumtion" })),
+                React.createElement("div", { className: "col-md-4" },
+                    React.createElement("label", { htmlFor: "fuelCosts" }, "fuel cost:")),
+                React.createElement("div", { className: "col-md-8" },
+                    React.createElement("input", { type: "number", value: this.props.fuelCosts, onChange: this.handleInputChange, id: "fuelCosts", name: "fuelCosts" })))));
+    }
+}
+exports.CostsForm = CostsForm;
 
 
 /***/ })
